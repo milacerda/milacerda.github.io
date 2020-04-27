7fd4bb07b6e94444526b8fb17dfa380c04c4f633
Camila Lacerda <milacerda@gmail.com>
2020-04-27 00:37:29 -0300

    init

---
+++ precache-manifest.c5bb022b11cd8ccaa43d85a5c5390de7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e83cf5486320c586bb6615ae1c550b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b1f582500015ed8561",
+    "url": "/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "fe382daaae8bcf481c82",
+    "url": "/static/css/main.0d0050d2.chunk.css"
+  },
+  {
+    "revision": "c2b1f582500015ed8561",
+    "url": "/static/js/2.7545a52e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.7545a52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe382daaae8bcf481c82",
+    "url": "/static/js/main.765af841.chunk.js"
+  },
+  {
+    "revision": "b0d04547bb0c1cd7d91f",
+    "url": "/static/js/runtime-main.90804fe5.js"
+  },
+  {
+    "revision": "e2595e9c2b81ebe92d93b6fd060fafaf",
+    "url": "/static/media/unicorn.e2595e9c.jpg"
+  }
+]);
